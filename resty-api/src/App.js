3f15d81662f1702c64021519d1215c51f4e96bcd
RivaD2 <rivkadavidowski@gmail.com>
2020-11-17 16:26:58 -0800

    working on fetching data from url

--- resty-api/src/App.js
+++ resty-api/src/App.js
@@ -6,11 +6,26 @@ import './App.css';
 
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      people: [],
+    }
+  }
+  async componentDidMount() {
+    const url = 'https://swapi.co/api/people'
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({person: data.results[0]})
+    console.log(data.results[0]);
+  }
+   
+  
   render() {
     return (
      <div className="App">
        <Header/>
-       <Form/>
+       <Form />
       <Footer/>
      </div>
     );
--- resty-api/src/Components/Header.js
+++ resty-api/src/Components/Header.js
@@ -7,15 +7,7 @@ class Header extends React.Component {
                 <div className="navTitle">
                 <div>RESTy-Api</div>
                 </div>
-                <div className="nav" id="mynav">
-                    <a href="#home" class="active">Home</a>
-                    <a href="#contact">Contact</a>
-                    <a href="#about">About</a>
                 </div>
-                <div className="burger-bar">
-                    <i class="FaBars"></i>
-                </div>
-            </div>
         )
     } 
 }
